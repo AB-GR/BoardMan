7acafa09814cd3545512c1ba0ec76b0a7b6b6d4e
Abhilash <abhilashgraja@gmail.com>
2022-02-13 09:36:17 +0530

    updated action tag helpers to use typed controller and action names

---
+++ BoardMan/BoardMan.Web/Utils/ControllerUtils.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoardMan.Web.Utils
+{
+	public class ControllerUtils
+	{
+		public static string GetControllerName<T>() where T : Controller
+		{
+			return typeof(T).Name.Replace(nameof(Controller), string.Empty);
+		}
+	}
+}
